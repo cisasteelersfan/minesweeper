1a36e4109efdc5bd0281688477b9c6234145678c
Colby Rome <cisasteelersfan@gmail.com>
2020-04-06 22:41:08 -0400

    Updates

---
+++ precache-manifest.1c45512a04f7cd9a84a859148a340d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5141791e966dda30ac6ccd0d9a933b80",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "9725085eaecd3b1d8ca2",
+    "url": "/minesweeper/static/css/main.278c6583.chunk.css"
+  },
+  {
+    "revision": "2381b10da2ddfb9b69f1",
+    "url": "/minesweeper/static/js/2.e3c927b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.e3c927b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9725085eaecd3b1d8ca2",
+    "url": "/minesweeper/static/js/main.bc852f89.chunk.js"
+  },
+  {
+    "revision": "e2c7e9f46b9595c0ecda",
+    "url": "/minesweeper/static/js/runtime-main.85a42047.js"
+  },
+  {
+    "revision": "0e0c9e644583fe034e32fe0fdc9a0d98",
+    "url": "/minesweeper/static/media/flag.0e0c9e64.svg"
+  }
+]);
